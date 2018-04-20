8ad4d570328bb23d0742b21884f60c52bdea3fc3
Gao Zhiyuan <alapha23@gmail.com>
2018-04-20 15:11:05 +0900

    added magic

---
+++ timit.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+
+if [ ! -d ./TIMIT ]; then
+        echo "I thought you had ./TIMIT "
+        exit 1
+fi 
+
+dir="./TIMIT/"
+te="./TIMIT/TEST"
+mv $dir/SX139.WAV $te/
+mv $dir/SX23.WAV $te/
+mv $dir/SX306.WAV $te/
+mv $dir/SX388.WAV $te/
+mv $dir/SX281.WAV $te/
+mv $dir/SX44.WAV $te/
+mv $dir/SX5.WAV $te/
+mv $dir/SX192.WAV $te/
+mv $dir/SX8.WAV $te/
+mv $dir/SX283.WAV $te/
+mv $dir/SX290.WAV $te/
+mv $dir/SX143.WAV $te/
+mv $dir/SX47.WAV $te/
+mv $dir/SX133.WAV $te/
+mv $dir/SX228.WAV $te/
+mv $dir/SX14.WAV $te/
+mv $dir/SX274.WAV $te/
+mv $dir/SX34.WAV $te/
+mv $dir/SX370.WAV $te/
+mv $dir/SX50.WAV $te/
+mv $dir/SX29.WAV $te/
+mv $dir/SX369.WAV $te/
+mv $dir/SX109.WAV $te/
+mv $dir/SX25.WAV $te/
+mv $dir/SX319.WAV $te/
+mv $dir/SX203.WAV $te/
+mv $dir/SX36.WAV $te/
+mv $dir/SX118.WAV $te/
+mv $dir/SX11.WAV $te/
+mv $dir/SX224.WAV $te/
+mv $dir/SX185.WAV $te/
+mv $dir/SX372.WAV $te/
+mv $dir/SX188.WAV $te/
+mv $dir/SX13.WAV $te/
+mv $dir/SX20.WAV $te/
+mv $dir/SX323.WAV $te/
+mv $dir/SX227.WAV $te/
+mv $dir/SX313.WAV $te/
+mv $dir/SX408.WAV $te/
+mv $dir/SX194.WAV $te/
+mv $dir/SX4.WAV $te/
+mv $dir/SX214.WAV $te/
+mv $dir/SX100.WAV $te/
+mv $dir/SX230.WAV $te/
+mv $dir/SX209.WAV $te/
+mv $dir/SX99.WAV $te/
+mv $dir/SX289.WAV $te/
+mv $dir/SX205.WAV $te/
+mv $dir/SX49.WAV $te/
+mv $dir/SX383.WAV $te/
+mv $dir/SX216.WAV $te/
+mv $dir/SX298.WAV $te/
+mv $dir/SX191.WAV $te/
+mv $dir/SX404.WAV $te/
+mv $dir/SX365.WAV $te/
+mv $dir/SX102.WAV $te/
+mv $dir/SX368.WAV $te/
+mv $dir/SX193.WAV $te/
+mv $dir/SX200.WAV $te/
+mv $dir/SX53.WAV $te/
+mv $dir/SX407.WAV $te/
+mv $dir/SX43.WAV $te/
+mv $dir/SX138.WAV $te/
+mv $dir/SX374.WAV $te/
+mv $dir/SX184.WAV $te/
+mv $dir/SX394.WAV $te/
+mv $dir/SX280.WAV $te/
+mv $dir/SX410.WAV $te/
+mv $dir/SX389.WAV $te/
+mv $dir/SX279.WAV $te/
+mv $dir/SX19.WAV $te/
+mv $dir/SX385.WAV $te/
+mv $dir/SX229.WAV $te/
+mv $dir/SX113.WAV $te/
+mv $dir/SX396.WAV $te/
+mv $dir/SX28.WAV $te/
+mv $dir/SX371.WAV $te/
+mv $dir/SX134.WAV $te/
+mv $dir/SX95.WAV $te/
+mv $dir/SX282.WAV $te/
+mv $dir/SX98.WAV $te/
+mv $dir/SX373.WAV $te/
+mv $dir/SX380.WAV $te/
+mv $dir/SX233.WAV $te/
+mv $dir/SX137.WAV $te/
+mv $dir/SX223.WAV $te/
+mv $dir/SX318.WAV $te/
+mv $dir/SX104.WAV $te/
+mv $dir/SX364.WAV $te/
+mv $dir/SX124.WAV $te/
+mv $dir/SX10.WAV $te/
+mv $dir/SX140.WAV $te/
+mv $dir/SX119.WAV $te/
+mv $dir/SX9.WAV $te/
+mv $dir/SX199.WAV $te/
+mv $dir/SX115.WAV $te/
+mv $dir/SX409.WAV $te/
+mv $dir/SX293.WAV $te/
+mv $dir/SX126.WAV $te/
+mv $dir/SX208.WAV $te/
+mv $dir/SX101.WAV $te/
+mv $dir/SX314.WAV $te/
+mv $dir/SX275.WAV $te/
+mv $dir/SX12.WAV $te/
+mv $dir/SX278.WAV $te/
+mv $dir/SX103.WAV $te/
+mv $dir/SX110.WAV $te/
+mv $dir/SX413.WAV $te/
+mv $dir/SX317.WAV $te/
+mv $dir/SX403.WAV $te/
+mv $dir/SX48.WAV $te/
+mv $dir/SX284.WAV $te/
+mv $dir/SX94.WAV $te/
+mv $dir/SX304.WAV $te/
+mv $dir/SX190.WAV $te/
+mv $dir/SX320.WAV $te/
+mv $dir/SX299.WAV $te/
+mv $dir/SX189.WAV $te/
+mv $dir/SX379.WAV $te/
+mv $dir/SX295.WAV $te/
+mv $dir/SI1039.WAV $te/
+mv $dir/SI923.WAV $te/
+mv $dir/SI756.WAV $te/
+mv $dir/SI838.WAV $te/
+mv $dir/SI731.WAV $te/
+mv $dir/SI944.WAV $te/
+mv $dir/SI905.WAV $te/
+mv $dir/SI642.WAV $te/
+mv $dir/SI908.WAV $te/
+mv $dir/SI733.WAV $te/
+mv $dir/SI740.WAV $te/
+mv $dir/SI1043.WAV $te/
+mv $dir/SI947.WAV $te/
+mv $dir/SI583.WAV $te/
+mv $dir/SI678.WAV $te/
+mv $dir/SI464.WAV $te/
+mv $dir/SI724.WAV $te/
+mv $dir/SI934.WAV $te/
+mv $dir/SI820.WAV $te/
+mv $dir/SI950.WAV $te/
+mv $dir/SI929.WAV $te/
+mv $dir/SI819.WAV $te/
+mv $dir/SI1961.WAV $te/
+mv $dir/SI925.WAV $te/
+mv $dir/SI1669.WAV $te/
+mv $dir/SI1553.WAV $te/
+mv $dir/SI1386.WAV $te/
+mv $dir/SI1473.WAV $te/
+mv $dir/SI1361.WAV $te/
+mv $dir/SI1272.WAV $te/
+mv $dir/SI1535.WAV $te/
+mv $dir/SI1574.WAV $te/
+mv $dir/SI1538.WAV $te/
+mv $dir/SI1363.WAV $te/
+mv $dir/SI1370.WAV $te/
+mv $dir/SI1673.WAV $te/
+mv $dir/SI1577.WAV $te/
+mv $dir/SI1213.WAV $te/
+mv $dir/SI1308.WAV $te/
+mv $dir/SI1094.WAV $te/
+mv $dir/SI1354.WAV $te/
+mv $dir/SI1564.WAV $te/
+mv $dir/SI1450.WAV $te/
+mv $dir/SI1580.WAV $te/
+mv $dir/SI1559.WAV $te/
+mv $dir/SI1449.WAV $te/
+mv $dir/SI1189.WAV $te/
+mv $dir/SI822.WAV $te/
+mv $dir/SI2299.WAV $te/
+mv $dir/SI2183.WAV $te/
+mv $dir/SI2016.WAV $te/
+mv $dir/SI2098.WAV $te/
+mv $dir/SI1991.WAV $te/
+mv $dir/SI2204.WAV $te/
+mv $dir/SI1791.WAV $te/
+mv $dir/SI1902.WAV $te/
+mv $dir/SI2168.WAV $te/
+mv $dir/SI1993.WAV $te/
+mv $dir/SI2000.WAV $te/
+mv $dir/SI2303.WAV $te/
+mv $dir/SI1584.WAV $te/
+mv $dir/SI1843.WAV $te/
+mv $dir/SI1938.WAV $te/
+mv $dir/SI602.WAV $te/
+mv $dir/SI1984.WAV $te/
+mv $dir/SI2194.WAV $te/
+mv $dir/SI2080.WAV $te/
+mv $dir/SI2210.WAV $te/
+mv $dir/SI2189.WAV $te/
+mv $dir/SI2079.WAV $te/
+mv $dir/SI1819.WAV $te/
+mv $dir/SI2185.WAV $te/
+
+mv $dir/*.WAC $dir/TRAIN
+
+#;ID   <SA>  <-------- SX --------->  <----- SI ----->
+#DAB0  1  2  139  319   49  229  409  1039  1669  2299
+#WBT0  1  2   23  203  383  113  293   923  1553  2183
+#ELC0  1  2  306   36  216  396  126   756  1386  2016
+#TAS1  1  2  388  118  298   28  208   838  1473  2098
+#WEW0  1  2  281   11  191  371  101   731  1361  1991
+#PAS0  1  2   44  224  404  134  314   944  1272  2204
+#JMP0  1  2    5  185  365   95  275   905  1535  1791
+#LNT0  1  2  192  372  102  282   12   642  1574  1902
+#PKT0  1  2    8  188  368   98  278   908  1538  2168
+#LLL0  1  2  283   13  193  373  103   733  1363  1993
+#TLS0  1  2  290   20  200  380  110   740  1370  2000
+#JLM0  1  2  143  323   53  233  413  1043  1673  2303
+#BPM0  1  2   47  227  407  137  317   947  1577  1584
+#KLT0  1  2  133  313   43  223  403   583  1213  1843
+#NLP0  1  2  228  408  138  318   48   678  1308  1938
+#CMJ0  1  2   14  194  374  104  284   464  1094   602
+#JDH0  1  2  274    4  184  364   94   724  1354  1984
+#MGD0  1  2   34  214  394  124  304   934  1564  2194
+#GRT0  1  2  370  100  280   10  190   820  1450  2080
+#NJM0  1  2   50  230  410  140  320   950  1580  2210
+#DHC0  1  2   29  209  389  119  299   929  1559  2189
+#JLN0  1  2  369   99  279    9  189   819  1449  2079
+#PAM0  1  2  109  289   19  199  379  1961  1189  1819
+#MLD0  1  2   25  205  385  115  295   925   822  2185
+
+
+
